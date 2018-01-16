2617214c478b286a4786aba4d91598efeeddaa19
Le Duy Khanh <ledu0003@e.ntu.edu.sg>
2018-01-17 04:07:02 +0800

    cart

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -47,7 +47,7 @@ class CartScreen extends Component {
   }
 
   render() {
-
+    let total = 0;
     return (
       <Container>
 
@@ -56,20 +56,24 @@ class CartScreen extends Component {
           <Content>
           {
             this.props.cart.items.map(
-              item =>
+              item => {
+                total += item.price * item.qty;
+                return(
                 <View horizontal center>
                   <Image resizeMode='stretch' style={{width: 100, height: 100}} source={{uri: item.image}}/>
                   <Text bold style={{flex: 2}}>{item.name}</Text>
-                  <View center-h style={{flex: 2}} >
+                  <View center-h style={{flex: 2}}>
                     <View horizontal light-border center space-between p-30 style={{alignSelf: 'stretch',}}>
-                      <Icon onPress={() => this.remove(item.id)} name="ios-remove" />
+                      <Icon onPress={() => this.remove(item.id)} name="ios-remove"/>
                       <Text>{item.qty}</Text>
-                      <Icon onPress={() => this.add(item.id)} name="ios-add" />
+                      <Icon onPress={() => this.add(item.id)} name="ios-add"/>
                     </View>
                   </View>
                   <Text style={{flex: 1}}>{item.price * item.qty}</Text>
-                  <Icon onPress={() => this.add(item.id)} name="ios-trash" />
+                  <Icon onPress={() => this.add(item.id)} name="ios-trash"/>
                 </View>
+                )
+              }
               )
           }
 
@@ -78,7 +82,7 @@ class CartScreen extends Component {
           <Footer>
             <FooterTab>
               <View m-l-10 center-h>
-                <Text theme fs18>${'456'}</Text>
+                <Text theme fs18>${total}</Text>
               </View>
               <View m-r-10 center center-h>
                 <Button small onPress={this.placeOrder.bind(this)}><Text>CHECK OUT</Text></Button>
